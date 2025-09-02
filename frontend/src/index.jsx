917253e74ce1076f5b5b1cf769989d450fbb07b3
Jake Andrade <jdeandrade@student.unimelb.edu.au>
2025-09-02 16:18:19 +1000

    Merge branch 'fix/setup-frontend'

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './pages/App';
-import reportWebVitals from './reportWebVitals';
+import App from './app/App';
+import reportWebVitals from './utils/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
